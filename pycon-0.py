740e1327cf67e7590996707f352b2f18fecf4b18
Chakrit Wichian <service@chakrit.net>
2023-12-14 10:45:57 +0700

    Add christmas tree example

--- pycon-0.py
+++ pycon-0.py
@@ -6,13 +6,10 @@ import dagger
 async def main():
     config = dagger.Config(log_output=sys.stderr)
     async with dagger.Connection(config) as client:
-        host = client.host()
-
         hello = (
             client.container()
             .from_('alpine:3.19')
-            .with_file('/hello.py', host.file(__file__))
-            .with_exec(['cat', '/hello.py'])
+            .with_exec(['echo', 'hello'])
         )
 
         print(await hello.stdout())
--- pycon-1.py
+++ pycon-1.py
@@ -2,14 +2,18 @@ import sys
 import anyio
 import dagger
 
+URL = "https://raw.githubusercontent.com/chicolucio/terminal-christmas-tree/master/terminal_tree.py"
+
 
 async def main():
     config = dagger.Config(log_output=sys.stderr)
     async with dagger.Connection(config) as client:
         hello = (
             client.container()
-            .from_('alpine:3.19')
-            .with_exec(['echo', 'hello'])
+            .from_('python:3.11-alpine')
+            .with_exec(['apk', 'add', '--no-cache', 'curl'])
+            .with_exec(['curl', '-o', 'tree.py', URL])
+            .with_exec(['python', 'tree.py'])
         )
 
         print(await hello.stdout())
